c9ecc6a62b6d228028386d1bf84d4bd913fc8ac8
Maksim <maksim.sokur90@gmail.com>
2015-09-11 20:42:45 +0300

    1

--- src/com/company/MAX/Week1_add_1_15.java
+++ src/com/company/MAX/Week1_add_1_15.java
@@ -11,13 +11,22 @@ public class Week1_add_1_15 {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Введите шплощадь круга: ");
-		int squareCicle = scanner.nextInt();
+		double squareCicle = scanner.nextDouble();
 
 		System.out.print("Введите шплощадь квадрата: ");
-		int squareRectangle = scanner.nextInt();
+		double squareRectangle = scanner.nextDouble();
 
 		double radiusKruga = Math.sqrt(squareCicle/ 3.1415);
+		double storonaKvadrata = Math.sqrt(squareRectangle);
 
+		double Rv = storonaKvadrata / 2;
+		double Ro = Math.sqrt(2)/2 * storonaKvadrata;
+
+		if (Rv > radiusKruga){
+			System.out.print("Круг поместится в квадрате!");
+		}else if (Ro < radiusKruga ){
+			System.out.print("Квадрат поместится в круге!");
+		}
 
 	}
 }
