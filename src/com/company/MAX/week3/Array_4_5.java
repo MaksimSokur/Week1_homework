7b1e9979996f64e9dfa0305c0b2d729eb5960941
Maksim <maksim.sokur90@gmail.com>
2015-09-13 19:37:32 +0300

    1

---
+++ src/com/company/MAX/week3/Array_4_5.java
@@ -0,0 +1,19 @@
+package com.company.MAX.week3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by root on 13.09.15.
+ */
+public class Array_4_5 {
+	public static void main(String[] args) {
+
+		String string = "array is data type";
+		System.out.print(string);
+		System.out.print('\n');
+		final String a = string.replace('a', '@');
+		System.out.print(a);
+
+	}
+}
